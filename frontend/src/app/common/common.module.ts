be8eaece9489442c98adc46462b8ba4fdf69441f
skg1998 <sahilkg1998@gmail.com>
2023-01-09 22:06:54 +0530

    initial setup

---
+++ frontend/src/app/common/common.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from './product-card/product-card.component';
+
+
+
+@NgModule({
+  declarations: [ProductCardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommonModule { }
